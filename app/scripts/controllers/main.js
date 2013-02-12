c420532eeac64cb82350d802f7f8cdb4c8b234cb
Vitor Balocco <vitorbal@gmail.com>
2013-02-12 22:27:17 +0100

    Comment out GA client code, change scope notation to work with minification

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-insightDashboardApp.controller('MainCtrl', function($scope) {
+insightDashboardApp.controller('MainCtrl', ['$scope', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Testacular'
   ];
-});
+}]);
