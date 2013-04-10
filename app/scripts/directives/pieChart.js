629cae50e3e49e36a6a80246caf24fad12c9c601
Vitor Balocco <vitorbal@gmail.com>
2013-04-10 23:52:33 +0200

    Start Using nvd3 for charts

--- app/scripts/directives/pieChart.js
+++ app/scripts/directives/pieChart.js
@@ -17,61 +17,30 @@ insightDashboardApp.directive('pieChart', function() {
     link: function postLink( scope, element, attrs ) {
       var width = +scope.width || 400;
       var height = +scope.height || 400;
-      var radius = Math.min( width, height ) / 2;
-
       var data = scope.data;
 
-      var arc = d3.svg.arc()
-          .outerRadius( radius - 10 )
-          .innerRadius( 0 );
+     var svg = d3.select( element[0] )
+        .append('svg')
+        .attr( 'width', width )
+        .attr( 'height', height )
 
-      var labelsArc = d3.svg.arc().outerRadius( arc.outerRadius() );
+      var chart = nv.models.pieChart()
+          .x( function(d) { return d.key } )
+          .y( function(d) { return d.value } )
+          .showLegend( false )
+          .values( function(d) { return d } )
+          .color( d3.scale.category10().range() )
+          .width( width )
+          .height( height )
+          .margin({ top: 0, bottom: 0 });
 
-      var pie = d3.layout.pie()
-          .sort( null )
-          .value( function(d) { return d.value; } );
-
-      var svg = d3.select( element[0] )
-          .append('svg')
-            .attr( 'class', 'pie-chart')
-            .attr( 'width', width )
-            .attr( 'height', height + 50 )
-          .append('g')
-            .attr( 'transform', 'translate(' + width / 2 + ',' + (height / 2 + 25) + ')');
 
       // Run this everytime scope.data changes
       scope.$watch( 'data', function( data ) {
-
-        var path = svg.selectAll('path')
-            .data( pie( data ) )
-            .attr( 'd', arc );
-
-        var text = svg.selectAll('text')
-            .data( pie( data ) )
-            .text( function(d) { return d.data.key; } )
-            .attr('transform', function(d) {
-              d.innerRadius = radius + 50;
-              d.outerRadius = radius;
-              return 'translate(' + labelsArc.centroid( d ) + ')';
-            });
-
-        path.enter()
-            .append('path')
-            .attr( 'fill', function(d) { return color( d.data.key ); } )
-            .attr( 'd', arc )
-
-        path.enter()
-            .append('text')
-            .style( 'text-anchor', 'middle' )
-            .text( function(d) { return d.data.key; } )
-            .attr('dy', '.35em')
-            .attr('transform', function(d) {
-              d.innerRadius = radius + 50;
-              d.outerRadius = radius;
-              return 'translate(' + labelsArc.centroid( d ) + ')';
-            });
-
-        path.exit().remove();
+        svg.datum( [data] )
+          .transition()
+          .duration( 1200 )
+          .call( chart );
 
       });
     }
